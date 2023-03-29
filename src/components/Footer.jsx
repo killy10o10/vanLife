2c949305e5b2e672a83ba426a01bb7bdd5e25cc7
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-03-29 01:59:51 +0000

    Style home component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 function Footer() {
-  return <footer>&copy; 2022 #VANLIFE</footer>;
+  const dateObj = new Date();
+  const currentYear = dateObj.getFullYear()
+  return <footer className="killy">DESIGNED BY: <a href="https://github.com/killy10o10" className="killy">Kingsley Okine / Quami killy</a> &copy; <a href="#">{currentYear}</a></footer>;
 }
 
 export default Footer;

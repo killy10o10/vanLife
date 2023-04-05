d92ab854163195c5cbef6110b11fb353c920a804
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-04-05 22:39:13 +0000

    Style van Card

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,11 +1,11 @@
-import vanImage from '../assets/van1.png'
+import vanImage from '../assets/van1.png';
 
 function Vans() {
   return (
     <section className="vans-section">
       <div className="category">
         <h3>Explore our van options</h3>
-          <small>Clear fileters</small>
+        <small>Clear fileters</small>
         <ul className="category-list">
           <li className="simple">Simple</li>
           <li className="luxury">Luxury</li>
@@ -17,30 +17,14 @@ function Vans() {
           <div className="van-image">
             <img src={vanImage} alt="van-image" />
             <div className="van-name">
-              <h3>Modest Explorer</h3>
+              <p>Modest Explorer</p>
+              <p>
+                $60 <small>/day</small>
+              </p>
             </div>
           </div>
           <div className="van-description">
-          </div>
-        </div>
-        <div className="van">
-          <div className="van-image">
-            <img src={vanImage} alt="van-image" />
-            <h3>Modest Explorer</h3>
-          </div>
-          <div className="van-description">
-            <div className="van-name">
-            </div>
-          </div>
-        </div>
-        <div className="van">
-          <div className="van-image">
-            <img src={vanImage} alt="van-image" />
-            <h3>Modest Explorer</h3>
-          </div>
-          <div className="van-description">
-            <div className="van-name">
-            </div>
+            <button className="category-button">Simple</button>
           </div>
         </div>
       </div>

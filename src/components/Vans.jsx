cd4c4ffeb3a4e027708dd44461982665e18cfa3f
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-04-28 04:09:10 +0000

    Add van details component
    Style van details

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,39 +1,41 @@
 import { useState, useEffect } from 'react';
 import '../../server';
+import { NavLink } from 'react-router-dom';
 
 function Vans() {
   const [vansData, setVansData] = useState([]);
 
   useEffect(() => {
     (async () => {
-      try{
+      try {
         const response = await fetch('/api/vans');
         const data = await response.json();
         setVansData(data.vans);
+      } catch (err) {
+        console.log('Error occured when fetching vans');
       }
-      catch(err){
-        console.log('Error occured when fetching vans')
-      }
-    })()
+    })();
   }, []);
 
-  const vanListEl = vansData.map((van) => 
-  <div className="van" key={van.id}>
-    <div className="van-image">
-      <img src={van.imageUrl} alt={van.name} />
-      <div className="van-name">
-        <p>{van.name}</p>
-        <p>
-          {van.price}
-          <small>/day</small>
-        </p>
+  const vanListEl = vansData.map((van) => (
+    <div className="van" key={van.id}>
+      <div className="van-image">
+        <NavLink to="/vanDetails" state={van}>
+          <img src={van.imageUrl} alt={van.name} />
+        </NavLink>
+        <div className="van-name">
+          <p>{van.name}</p>
+          <p>
+            {van.price}
+            <small>/day</small>
+          </p>
+        </div>
+      </div>
+      <div className="van-description">
+        <button className={`category-button ${van.type}`}>{van.type}</button>
       </div>
     </div>
-    <div className="van-description">
-      <button className="category-button">{van.type}</button>
-    </div>
-  </div>
-);
+  ));
 
   return (
     <section className="vans-section">
@@ -46,9 +48,7 @@ function Vans() {
           <li className="rugged">Rugged</li>
         </ul>
       </div>
-      <div className="vans">
-        { vanListEl || `Loading...`}
-      </div>
+      <div className="vans">{vanListEl || `Loading...`}</div>
     </section>
   );
 }

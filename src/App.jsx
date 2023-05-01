3774dc3cd73bf663f720fa99994fd0c812d30fff
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-05-01 03:42:02 +0000

    Add layout component
    implement nested routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Vans from './components/Vans';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Footer from './components/Footer';
-import VanDetails from './components/vanDetails';
+import Home from './pages/Home';
+import Vans from './pages/Vans';
+import About from './pages/About';
+import VanDetails from './pages/vanDetails';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vanDetails" element={<VanDetails />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vanDetails" element={<VanDetails />} />
+        </Route>
       </Routes>
-      <Footer />
     </>
   );
 }

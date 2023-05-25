c2baf8999e577fa19e392e93113fa55a1d4fe178
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-05-25 16:41:52 +0000

    Dynamiclly render host vans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,31 +1,44 @@
-import vanPicture from '../../assets/van1.png';
+import '../../../server';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const HostVans = () => {
+  const [vans, setVans] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('/api/vans');
+        const data = await response.json();
+        setVans(data.vans);
+      } catch (err) {
+        console.log('Error occurred when fetching vans');
+      }
+    })();
+  }, []);
+
+  const hostVans = vans.map((van) => {
+    return (
+      <Link to={`/host/vans/${van.hostId}`} key={van.id}>
+        <div className="host-van">
+          <div className="host-van-image">
+            <img src={van.imageUrl} alt={van.name} />
+          </div>
+          <div className="host-van-desc">
+            <h4>{van.name}</h4>
+            <small>${van.price}/day</small>
+          </div>
+        </div>
+      </Link>
+    );
+  });
+
   return (
     <>
       <section className="host-vans-section">
         <h3>Your Listed vans</h3>
         <div className="host-vans-container">
           {/* Host Van */}
-          <div className="host-van">
-            <div className="host-van-image">
-              <img src={vanPicture} alt="vanImage" />
-            </div>
-            <div className="host-van-desc">
-              <h4>Modest Explorer</h4>
-              <small>$60/day</small>
-            </div>
-          </div>
-          {/* Host Van */}
-          <div className="host-van">
-            <div className="host-van-image">
-              <img src={vanPicture} alt="vanImage" />
-            </div>
-            <div className="host-van-desc">
-              <h4>Modest Explorer</h4>
-              <small>$60/day</small>
-            </div>
-          </div>
+            {hostVans}
         </div>
       </section>
     </>

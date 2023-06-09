883173a21ba061a4c23a05aceebc5b57a8b286a2
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-06-09 22:38:29 +0000

    wip

--- src/pages/Vans/vanDetails.jsx
+++ src/pages/Vans/vanDetails.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
+import { motion } from 'framer-motion';
 
 function VanDetails() {
   const location = useLocation();
@@ -10,21 +11,23 @@ function VanDetails() {
       <Link to="/vans" className="back">
         <BsArrowLeft /> Back to all vans
       </Link>
-      <div className="van-details-image">
-        <img src={state.imageUrl} alt={state.name} />
-      </div>
-      <div className="van-details-description">
-        <button className={`category-button ${state.type}`}>
-          {state.type}
-        </button>
-        <h3>{state.name}</h3>
-        <p className="price">
-          {state.price}
-          <small>/day</small>
-        </p>
-        <p>{state.description}</p>
-        <button className="button">Rent this van</button>
-      </div>
+      <motion.div layout className="van-details-grid">
+        <div className="van-details-image">
+          <img src={state.imageUrl} alt={state.name} />
+          <button className={`category-button ${state.type}`}>
+            {state.type}
+          </button>
+        </div>
+        <div className="van-details-description">
+          <h3>{state.name}</h3>
+          <p className="price">
+            {state.price}
+            <small>/day</small>
+          </p>
+          <p>{state.description}</p>
+          <button className="button">Rent this van</button>
+        </div>
+      </motion.div>
     </section>
   );
 }
